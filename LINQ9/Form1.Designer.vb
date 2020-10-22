46fdd43bd6420e0527d81f28d345c0ac83b75b5e
Miguel Basilio <lokoangel06@gmail.com>
2020-10-21 20:24:06 -0700

    Add project files.

---
+++ LINQ9/Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(34, 16)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(96, 41)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "create new file"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(136, 16)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(93, 41)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "create new file #2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(239, 16)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(92, 41)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "New File #3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(337, 16)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(92, 41)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "New File #4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(435, 16)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(92, 41)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "New File #5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(533, 16)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(92, 41)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "New File #6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(34, 63)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(223, 37)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "Display Scores"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 106)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(581, 168)
+        Me.DataGridView1.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(646, 289)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ LINQ9/Form1.vb
@@ -0,0 +1,121 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+
+            Dim query = From line In IO.File.ReadAllLines("Justices.txt")
+                        Let data = line.Split(",")
+                        Let firstname = data(0)
+                        Let lastname = data(1)
+                        Let Fullname = firstname & "," & lastname
+                        Let State = data(3)
+                        Select Fullname & "," & State
+
+
+            'write lines
+            IO.File.WriteAllLines("Justice.txt", query)
+
+            MessageBox.Show("Data transfered")
+
+
+        Catch ex As Exception
+
+            MessageBox.Show(ex.ToString)
+
+        End Try
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim query = From line In IO.File.ReadAllLines("Justices.txt")
+                        Let data = line.Split(",")
+                        Let firstname = data(0)
+                        Let Lastname = data(1)
+                        Let fullname = (firstname & "," & Lastname).ToUpper
+                        Let yrAppointed = CInt(data(4))
+                        Select fullname & " was appointed in " & yrAppointed
+
+
+            IO.File.WriteAllLines("Just.txt", query)
+            MessageBox.Show("Data Transfered")
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim query = From line In IO.File.ReadAllLines("Justices.txt")
+                    Let lastNAme = line.Split(",")(1)
+                    Let pres = line.Split(",")(2)
+                    Let presLastName = pres.Split(" ").Last
+                    Let phrase = lastNAme & " was appointed by " & presLastName
+                    Select phrase
+
+
+        IO.File.WriteAllLines("pres.txt", query)
+        MessageBox.Show("Done")
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim query = From line In IO.File.ReadAllLines("Justices.txt")
+                    Let lastNAme = line.Split(",")(1)
+                    Let yrAppointed = CInt(line.Split(",")(4))
+                    Let years = Today.Year - yrAppointed
+                    Let phrase = lastNAme & " appointed  " & years & " years ago"
+                    Select phrase
+
+
+        IO.File.WriteAllLines("years.txt", query)
+        MessageBox.Show("Done")
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim query = From line In IO.File.ReadAllLines("Justices.txt")
+                    Let data = line.Split(",")
+                    Let firstname = data(0)
+                    Let Lastname = data(1)
+                    Let yrAppointed = CInt(data(4))
+                    Let newLine = Lastname & ", " & firstname & "," & yrAppointed
+                    Select newLine
+
+
+        IO.File.WriteAllLines("NewLine.txt", query)
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim query = From line In IO.File.ReadAllLines("Justices.txt")
+                    Let data = line.Split(",")
+                    Let lastName = data(1)
+                    Let pres = data(2)
+                    Let state = data(3)
+                    Let presLastName = pres.Split(" ").Last
+                    Let newLine = presLastName & ", " & lastName & ", " & state
+                    Select newLine
+
+
+        IO.File.WriteAllLines("NewLine.txt", query)
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim games() As String = IO.File.ReadAllLines("SuperBowelWinners.txt")
+
+        Dim query = From line In games
+                    Let data = line.Split(",")
+                    Let year = data(0)
+                    Let teamName = data(2)
+                    Let teamName2 = data(3)
+                    Let finalScore = data(4)
+                    Let location = data(5)
+                    Select year, teamName, teamName2, finalScore, location
+
+        DataGridView1.DataSource = query.ToList
+        DataGridView1.Columns("year").HeaderText = "Year"
+        DataGridView1.Columns("teamName").HeaderText = "Team Name 1"
+        DataGridView1.Columns("teamName2").HeaderText = "Team Name 2"
+        DataGridView1.Columns("finalScore").HeaderText = "Final Score"
+        DataGridView1.Columns("location").HeaderText = "Location"
+
+    End Sub
+End Class
